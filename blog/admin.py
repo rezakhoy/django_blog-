a1656d6b0fa1413b9c4e3574b72aae6e586765bb
rezakhoy <Reza@206200>
2020-04-04 14:14:15 +0430

    make model add to admin panel blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(UserProfile)
+admin.site.register(Article)
+admin.site.register(Category)
 
-# Register your models here.
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from ckeditor.fields import RichTextField
+
+
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    validate_extensions = ['.jpg', '.png']
+    if not ext.lower() in validate_extensions:
+        raise ValidationError('Unsupported file')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar', null=False, blank=False,
+                              validators=[validate_file_extension])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/article_cover', null=False, blank=False,
+                             validators=[validate_file_extension])
+    content = RichTextField(config_name='awesome_ckeditor')
+    created_at = models.DateTimeField(default=datetime.now())
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/category_cover', null=False, blank=False,
+                             validators=[validate_file_extension])
